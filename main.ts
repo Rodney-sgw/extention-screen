69027e8b233653bc003b7ae8c48f129f16552e4b
Rodney-sgw <72943959+Rodney-sgw@users.noreply.github.com>
2023-11-09 22:17:08 +0100

    Update pxt.json, main.ts, test.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,14 @@
-basic.forever(function () {
-	
-})
+namespace screenMagic {
+/*
+* LED vypln po sloupcich 
+*/
+//% blockId = screenmagicswitchit
+//% block = plot at i
+//% i.min = 0 i.max = 25
+
+    export function switchIt(i: number): void {
+        const y = Math.floor(i % 5);
+        const x = Math.floor(i / 5);
+        led.plot(x,y);  
+    }
+}
