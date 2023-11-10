8662de45805c6d9d905d75119cb5669f06a25a76
Rodney-sgw <72943959+Rodney-sgw@users.noreply.github.com>
2023-11-10 21:07:42 +0100

    Update main.ts

--- main.ts
+++ main.ts
@@ -2,9 +2,9 @@ namespace screenMagic {
 /*
 * LED vypln po sloupcich 
 */
-//% blockId=screenmagicswitchit
-//% block="rozsvit LED"
-//% i.min=0 i.max = 25
+//% blockId = screenmagicswitchit
+//% block = "rozsvit LED"
+//% i.min = 0 i.max = 25
 
     export function switchIt(i: number): void {
         const y = Math.floor(i % 5);
